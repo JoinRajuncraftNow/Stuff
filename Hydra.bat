cc9d37af21715b0503f3fcd5af79aeccbb84311b
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-11 17:19:42 -0500

    Update Hydra.bat

--- Hydra.bat
+++ Hydra.bat
@@ -2,6 +2,7 @@
 ::Powershell isn't blocked. Also, we have D and C++. XD
 ::What do you mean? I still have JScript!
 ::Is ps blocked, or possible?
+::How did you get batch blocked? (And D and C++ goto exe, which is very suspicious, JScript is .js, which looks like normal javascript.
 ::There is a way to check for comparisons, just not using < and >, since they are redirect chars
 TITLE Hydra 
 set n=0
