16d0387a0f07711acd1d1d78eb5ec0c87958f939
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-19 14:35:49 -0500

    Re-fix
    Don't feel like messing with the whole title thing right now, so.

--- Beeping/main.bat
+++ Beeping/main.bat
@@ -4,7 +4,7 @@ ECHO 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq volume" ^| find/c "exe"`) DO (
 SET windowcount=%%F
 )
-taskkill /FI "WINDOWTITLE ne main"
+taskkill /FI "WINDOWTITLE eq Command Prompt"
 IF "%windowcount%"=="0" (
 START volume.bat
 START main.bat
