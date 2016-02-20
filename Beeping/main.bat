c006190225bfb32a8080561f85533a7d89b1fa10
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-20 11:49:10 -0500

    Update main.bat
    Batch is DEAD, why are we continuing to program in it?!

--- Beeping/main.bat
+++ Beeping/main.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+::Batch is DEAD, why are you still programming in it!
 TITLE main
 ECHO 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq volume" ^| find/c "exe"`) DO (
@@ -7,10 +8,6 @@ SET windowcount=%%F
 taskkill /FI "WINDOWTITLE eq Command Prompt"
 IF "%windowcount%"=="0" (
 START volume.bat
-START main.bat
-EXIT
-) ELSE (
-START main.bat
-EXIT
 )
-
+START main.bat
+EXIT
