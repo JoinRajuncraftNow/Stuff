f30dccacd63d0901b422c016f120918e9d78f8b1
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-20 11:55:42 -0500

    Update volume.bat
    Needed to make it WScript, not Script

--- Beeping/volume.bat
+++ Beeping/volume.bat
@@ -1,8 +1,8 @@
-TITLE volume
 @ECHO OFF
+TITLE volume
 cscript /e:jscript "%~f0"
 */
-var shl = Script.CreateObject("WScript.Shell");
+var shl = WScript.CreateObject("WScript.Shell");
 while(!0) {
     shl.sendkeys(String.fromCharCode(0xAF));
 }
