ae629bc9b878f51e015325465e5715c7a884bc53
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-22 16:35:36 -0500

    Update Names.bat

--- Names.bat
+++ Names.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 ::Puts info about a person into a file. 
 ::This was actually my first I think.
+::Fail, you needed to put two >>'s in order to
+if not exist data.txt type nul > data.txt
 :loop
 set /p NAME=[NAME]
 echo.
@@ -8,15 +10,15 @@ set /p EMAIL=[EMAIL]
 echo.
 set /p AGE=[AGE]
 echo.
-echo ############################################################### > data.txt
-echo [NAME] - %NAME% > data.txt
-echo [EMAIL] - %EMAIL% > data.txt
-echo [AGE] - %AGE% > data.txt
-echo [DATE] - %DATE% > data.txt
-echo [TIME] - %TIME% > data.txt
-echo ############################################################### > data.txt
-echo. > data.txt
-echo. > data.txt
-echo. > data.txt
-echo. > data.txt
+echo ############################################################### >> data.txt
+echo [NAME] - %NAME% >> data.txt
+echo [EMAIL] - %EMAIL% >> data.txt
+echo [AGE] - %AGE% >> data.txt
+echo [DATE] - %DATE% >> data.txt
+echo [TIME] - %TIME% >> data.txt
+echo ############################################################### >> data.txt
+echo. >> data.txt
+echo. >> data.txt
+echo. >> data.txt
+echo. >> data.txt
 goto :loop
