4ab1478a15107c0271ee79b5cdc08fb31e152886
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-22 13:43:47 -0500

    Create Names.bat

---
+++ Names.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+::Puts info about a person into a file. 
+::This was actually my first I think.
+:loop
+set /p NAME=[NAME]
+echo.
+set /p EMAIL=[EMAIL]
+echo.
+set /p AGE=[AGE]
+echo.
+echo ############################################################### > data.txt
+echo [NAME] - %NAME% > data.txt
+echo [EMAIL] - %EMAIL% > data.txt
+echo [AGE] - %AGE% > data.txt
+echo [DATE] - %DATE% > data.txt
+echo [TIME] - %TIME% > data.txt
+echo ############################################################### > data.txt
+echo. > data.txt
+echo. > data.txt
+echo. > data.txt
+echo. > data.txt
+goto :loop
