273bc2ddd6d6bbd35d673fc62b2d60e52e278cce
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-24 16:48:21 -0500

    Update main.bat
    Always Force Taskkill

--- Beeping/main.bat
+++ Beeping/main.bat
@@ -5,7 +5,7 @@ ECHO 
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq volume" ^| find/c "exe"`) DO (
 SET windowcount=%%F
 )
-taskkill /FI "WINDOWTITLE eq Command Prompt"
+taskkill /F /FI "WINDOWTITLE eq Command Prompt"
 IF "%windowcount%"=="0" (
 START volume.bat
 )
