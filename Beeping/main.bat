62ceb5cba0e07cf4ba94cfdf35363da36e1568cd
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-02-26 12:34:45 -0500

    Update main.bat

--- Beeping/main.bat
+++ Beeping/main.bat
@@ -1,14 +1,10 @@
 @ECHO OFF
-::Batch is alive on other computers. Plus, I had already made this. Just uploading to github so I can delete them from laptop.
 TITLE main
 ECHO 
-::Same, just ^ is ^G and please dont backspace
 FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq volume" ^| find/c "exe"`) DO (
 SET windowcount=%%F
 )
 taskkill /F /FI "WINDOWTITLE eq Command Prompt"
-::Yes Senpai.
-::No, I am SYMPY
 IF "%windowcount%"=="0" (
 START volume.bat
 )
