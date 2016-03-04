09eecb0efc58662e9f12f31620d782f1ab58a552
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-04 14:10:59 -0500

    Capitalize

--- Beeping/main.bat
+++ Beeping/main.bat
@@ -2,8 +2,8 @@
 TITLE main
 ECHO 
 ::^G, please don't erase this comment
-taskkill /F /FI "WINDOWTITLE ne main"
-FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq volume" ^| find/c "exe"`) DO (
+TASKKILL /F /FI "WINDOWTITLE ne main"
+FOR /F "tokens=* USEBACKQ" %%F IN (`TASKLIST /FI "WINDOWTITLE eq volume" ^| FIND/C "exe"`) DO (
 SET windowcount=%%F
 )
 IF "%windowcount%"=="0" (
