936028e4b6f3769b144044bf40363976b183faf3
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-07 16:17:11 -0500

    Update TextAdventure.cpp

--- TextAdventure.cpp
+++ TextAdventure.cpp
@@ -810,7 +810,7 @@ int main()
 	x=0;
 	y=0;
 	z=4;
-    cout << endl << "This is a text adventure by Alex Trahan." << endl << "Defeat the Boss at (99,99,0) to win!" << endl << "Hit any key to continue." << endl;
+    cout << endl << "This is a text adventure by Alex Trahan, and very minor contributions by Zachary Taylor." << endl << "Defeat the Boss at (99,99,0) to win!" << endl << "Hit any key to continue." << endl;
     getline(cin, pause);
 	int health=1000, l=0;
 	while(true) //Never stop looping
