5706ded4c7806bb8cb9fb102b8e49192a2c9a6ea
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-03-21 09:27:32 -0400

    Fixed that...
    Just outputs time from epoch (I think thats right)

--- Pet.cpp
+++ Pet.cpp
@@ -8,12 +8,17 @@ using namespace std;
 This program is meant to be an interactive pet that keeps track of time with text files. Huge WIP, finish later.
 */
 
-int main()
+int whattime()
 {
 	time_t t=time(0);
+	return t;
+}
+
+int main()
+{
+	int health=1000, sick=0, hunger=0;
 	while(true)
 	{
-		cout << t << endl;
+		cout << whattime() << endl;
 	}
-	int health=1000, sick=0, hunger=0, time=0;
 }
