0f14679096f06b0eef8c6c4e8611b64d273ec35a
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-03-21 10:31:48 -0400

    Update Pet.cpp

--- Pet.cpp
+++ Pet.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+int whattime()
+{
+	time_t t=time(0);
+	return t;
+}
+
 int read()
 {
 	int n;
@@ -15,23 +21,15 @@ int read()
 	}
 }
 
-lines=i-1;
-}
 /*
 This program is meant to be an interactive pet that keeps track of time with text files. Huge WIP, finish later.
 */
-
-int whattime()
-{
-	time_t t=time(0);
-	return t;
-}
 
 int main()
 {
 	int health=1000, sick=0, hunger=0;
 	while(true)
 	{
-		cout << whattime() << endl;
+		cout << read() << endl;
 	}
 }
