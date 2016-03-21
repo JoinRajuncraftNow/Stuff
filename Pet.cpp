400f16f43f2715c508649469e32ec043999c7d2c
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-03-21 09:24:56 -0400

    This might be cool to make...

---
+++ Pet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+/*
+This program is meant to be an interactive pet that keeps track of time with text files. Huge WIP, finish later.
+*/
+
+int main()
+{
+	time_t t=time(0);
+	while(true)
+	{
+		cout << t << endl;
+	}
+	int health=1000, sick=0, hunger=0, time=0;
+}
