0469e2db4602673f7e3d68658f61f88fda3c66fa
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-02 19:17:20 -0400

    Update AIDevelopment.cpp

--- C++/AIDevelopment.cpp
+++ C++/AIDevelopment.cpp
@@ -1,14 +1,17 @@
 //Just a fragment of a file, so it will not work alone
-bool has_quest(int qx,int qy,int qz)
+string movef(int x,int y,int z,int qx,int qy,int qz,int num)
 {
-}
-bool can_move(int num)
-{
-  return num==10;
-}
-
-void movef()
-{
+  if(qx==100)
+  {
+    qx=99;
+    qy=99;
+    qz=0;
+  }
+  if(num==10&&qz!=z)
+  {
+    if(qz>z) return "u";
+    return "d";
+  }
 }
 
 void movec()
