0343f53f6aea845b4391c8b509b2ffad3a004810
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-02 19:44:38 -0400

    Update AIVersion.cpp

--- C++/AIVersion.cpp
+++ C++/AIVersion.cpp
@@ -15,6 +15,71 @@ Everyone is free to spot bugs and such. After all, it is on github.
 Check out the planned features.
 If you have any suggestions, or just want to contact me. My email is thecajunthatrages@gmail.com
 */
+//Just a fragment of a file, so it will not work alone
+void wait()
+{
+  system("sleep 1"); //Have to love universal commands
+}
+string movef_(int x,int y,int z,int qx,int qy,int qz,int num)
+{
+  if(qx==100)
+  {
+    qx=99;
+    qy=99;
+    qz=0;
+  }
+  if(num==10&&qz!=z)
+  {
+    if(qz>z) return "u";
+    return "d";
+  }
+  if(qy!=y)
+  {
+    if(qy>y) return "n";
+    return "s";
+  }
+  if(qx!=x)
+  {
+    if(qx>x) return "e";
+    return "w";
+  }
+  string c[4];
+  c[0]="n";c[1]="s";c[2]="w";c[3]="e";
+  int j=gen(0,3);
+  return c[j];
+}
+void movef(int& att,int x,int y,int z,int qx,int qy,int qz,int num)
+{
+  att=movef(x,y,z,qx,qy,qz,num);
+  wait();
+}
+string movec_(int health,int def,int m,int charge)
+{
+  int damage=200;
+  if(m>0)
+  {
+    if(health<=damage-def)
+    {
+      return "m";
+    }
+    if(charge)
+    {
+      return "d";
+    }
+    return "b";
+  }
+  if(charge) //We're screwed!
+  {
+    return "d";
+  }
+  return "b";
+}
+void movec(int& att,int health,int def,int m,int charge)
+{
+  att=movec(health,def,m,charge);
+  wait(); //Should wait
+}
+
 bool exists (string name) {
     ifstream f(name.c_str());
     if (f.good()) {
