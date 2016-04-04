b27d16f14872aa30b607d151c48c7c89b5a6f8ce
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-04 08:30:01 -0400

    FIXED GRAMMAR

--- C++/TextAdventureHardcore.cpp
+++ C++/TextAdventureHardcore.cpp
@@ -863,7 +863,7 @@ void questcomplete(int& m, int& def, int& quest, int& qx, int& qy, int& qz)
 { 
 	cout << "$$$$$$$$$$$$$$$$$$$$" << endl << "You have completed a quest!" << endl;
 	int r=gen(1,10);
-	cout << "You recieved " << r << " Medkits"; //semicolon
+	cout << "You recieved " << r << " Medkit" << (r==1 ? "" : "s"); //semicolon
 	m+=r; 
 	r=gen(1,10);
 	cout << " and " << r << " Armor!" << endl << "$$$$$$$$$$$$$$$$$$$$" << endl;
