ebedf35845b52f652d92cb9a93a3691add47b5b7
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-05 16:19:09 -0400

    Update Hardcore_Infinite.d

--- C++/Hardcore_Infinite.d
+++ C++/Hardcore_Infinite.d
@@ -3,3 +3,22 @@ bool exists(string name)
 	import std.file : exists;
 	return exists(name);
 }
+void save_others(int health, int m, int def, int x, int y, int z, int qx, int qy, int qz)
+{
+	import std.file : File;
+	import std.conv : to;
+	auto outf = File("save_file.txt","w");
+	void write(string arg)
+	{
+		outf.writeln(arg);
+	}
+	write(health);write(m);write(def);write(x);write(y);write(z);write(qx);write(qy);write(qz);
+	outf.close();
+}
+void load_others(ref int health,ref int m,ref int health,ref int def,ref int x,ref int y,ref int z,ref int qx,ref int qy,ref int qz)
+{
+	if(!exists("save_file.txt"))
+	{
+		return;
+	}
+}
