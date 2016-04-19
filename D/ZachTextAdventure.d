f82a19c8be4dd735bd15707d56cbe318479c4f94
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-19 16:47:58 -0400

    I am starting my own, Alex!

---
+++ D/ZachTextAdventure.d
@@ -0,0 +1,11 @@
+void info_commands()
+{
+  import std.stdio;
+  writeln("H - Help");
+  writeln("W - North");
+  writeln("A - West");
+  writeln("S - South");
+  writeln("D - East");
+  writeln("Q - Up");
+  writeln("Z - Down");
+}
