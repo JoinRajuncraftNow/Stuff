58209decaacc31bf12c5bc88b7eb65c7ab571ea4
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-19 21:06:14 -0400

    Update utils.js

--- D/Zach/HTML/utils.js
+++ D/Zach/HTML/utils.js
@@ -1,3 +1,4 @@
+var IS_READY
 function write(arg)
 {
   document.getElementById("ui").innerHTML+=arg;
@@ -5,4 +6,16 @@ function write(arg)
 function writeln(arg)
 {
   write(arg+"<br></br>");
+}
+function readln()
+{
+  while(true)
+  {
+    if(IS_READY)
+    {
+      IS_READY=false;
+      return document.getElementById("").value;
+    }
+    setTimeout(function(){},250);
+  }
 }
