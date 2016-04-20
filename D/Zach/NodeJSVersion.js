5f8cccf72cb09136fc9e1d543c7c9882a9fd932f
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-20 05:41:58 -0400

    Update NodeJSVersion.js

--- D/Zach/NodeJSVersion.js
+++ D/Zach/NodeJSVersion.js
@@ -1,3 +1,19 @@
+function clear(){}
 function info_map()
 {
+  console.log("H - Help");
+  console.log("W - North");
+  console.log("A - West");
+  console.log("S - South");
+  console.log("D - East");
+  console.log("Q - Up");
+  console.log("Z - Down");
+  console.log("M - Medkit");
+}
+function info_bottle()
+{
+  console.log("B-Big Slash: 15-50 + `pow` damage, 25% chance of missing");
+  console.log("Q-Quick Slash: 5-12 + `pow` damage, 10% chance of missing");
+  console.log("D-Dodge: 75% chance of dodging all damage");
+  console.log("M-Medkit: Restores you to full health");
 }
