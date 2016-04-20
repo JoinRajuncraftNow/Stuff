27a34b72d1348267b0cef8ddc0f83e09ecb6415f
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-20 05:23:58 -0400

    Update DVersion.d

--- D/Zach/DVersion.d
+++ D/Zach/DVersion.d
@@ -13,4 +13,12 @@ void info_map()
   writeln("Z - Down");
   writeln("M - Medkit");
 }
+void info_battle()
+{
+  import std.stdio;
+  writeln("B-Big slash: 15-50 damage, 25% chance of missing");
+  writeln("Q-Quick Slash: 5-12 damage, 10% chance of missing");
+  writeln("D-Dodge: 75% chance of dodgin all damage");
+  writeln("M-Medkit: Restores you to full health");
+}
 void clear(){} //This is kept in the different documents for compatibility with JS
