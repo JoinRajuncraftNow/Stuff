77d615554044ac6ff70645d6344c1fd94f6d9100
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-19 20:39:30 -0400

    Create JScript.js

---
+++ D/Zach/JScript.js
@@ -0,0 +1,10 @@
+function info_map()
+{
+  WScript.Echo("H - Help");
+  WScript.Echo("W - North");
+  WScript.Echo("A - West");
+  WScript.Echo("S - South");
+  WScript.Echo("D - East");
+  WScript.Echo("Q - Up");
+  WScript.Echo("Z - Down");
+}
