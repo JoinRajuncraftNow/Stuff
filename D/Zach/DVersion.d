7e0751e5e97a551de934fb6c3b34f243fd902e5d
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-20 05:52:35 -0400

    Update DVersion.d

--- D/Zach/DVersion.d
+++ D/Zach/DVersion.d
@@ -21,4 +21,5 @@ void info_battle()
   writeln("Q-Quick Slash: 5-12 + `pow` damage, 10% chance of missing");
   writeln("D-Dodge: 75% chance of dodging all damage");
   writeln("M-Medkit: Restores you to full health");
+  writeln("H-Help: Display this help message");
 }
