f63d5f56205a823514e517dd0dc6963b84101662
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-20 06:03:08 -0400

    Update PowershellVersion.ps1

--- D/Zach/PowershellVersion.ps1
+++ D/Zach/PowershellVersion.ps1
@@ -1,14 +1,23 @@
-function info_map
-{
-  echo "H - Help";
-  echo "W - North";
-  echo "A - West";
-  echo "S - South";
-  echo "D - East";
-  echo "Q - Up";
-  echo "Z - Down";
-}
 function clear
 {
   cls;
+}
+function info_map
+{
+  Write-Host "H - Help";
+  Write-Host "W - North";
+  Write-Host "A - West";
+  Write-Host "S - South";
+  Write-Host "D - East";
+  Write-Host "Q - Up";
+  Write-Host "Z - Down";
+  Write-Host "M - Medkit";
+}
+function info_battle
+{
+  Write-Host "B-Big Slash: 15-50 + ``pow`` damage, 25% chance of missing";
+  Write-Host "Q-Quick Slash: 5-12 + ``pow`` damage, 10% chance of missing";
+  Write-Host "D-Dodge: 75% chance of dodging all damage";
+  Write-Host "M-Medkit: Restores you to full health";
+  Write-Host "H-Help: Display this help message";
 }
