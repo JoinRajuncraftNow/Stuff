34641c70ff80d1ab33bf015fd32fef31a06c0391
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-19 20:21:22 -0400

    Create CppVersion.cpp

---
+++ D/Zach/CppVersion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+void info_map()
+{
+  cout << "H - Help" << endl;
+  cout << "W - North" << endl;
+  cout << "A - West" << endl;
+  cout << "S - South" << endl;
+  cout << "D - East" << endl;
+  cout << "Q - Up" << endl;
+  cout << "Z - Down" << endl;
+}
