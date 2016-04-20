29b13b01ccb91a670213c56c978809ea38a05b01
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-20 05:45:31 -0400

    Update CppVersion.cpp

--- D/Zach/CppVersion.cpp
+++ D/Zach/CppVersion.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+void clear(){}
 void info_map()
 {
   cout << "H - Help" << endl;
@@ -10,4 +11,10 @@ void info_map()
   cout << "Q - Up" << endl;
   cout << "Z - Down" << endl;
 }
-void clear(){}
+void info_battle()
+{
+  cout << "B-Big Slash: 15-50 + `pow` damage, 25% chance of missing" << endl;
+  cout << "Q-Quick Slash: 5-12 + `pow` damage, 10% chance of missing" << endl;
+  cout << "D-Dodge: 75% chance of dodging all damage" << endl;
+  cout << "M-Medkit: Restores you to full health" << endl;
+}
