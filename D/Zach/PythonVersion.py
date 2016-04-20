fa1be6802f94b358c458ae5bc0a7fa9159950209
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-20 06:05:19 -0400

    Update PythonVersion.py

--- D/Zach/PythonVersion.py
+++ D/Zach/PythonVersion.py
@@ -1,3 +1,5 @@
+def clear():
+  pass
 def info_map():
   print("H - Help")
   print("W - North")
@@ -6,5 +8,9 @@ def info_map():
   print("D - East")
   print("Q - Up")
   print("Z - Down")
-def clear():
-  pass
+def info_battle():
+  print("B-Big Slash: 15-50 + `pow` damage, 25% chance of missing")
+  print("Q-Quick Slash: 5-12 + `pow` damage, 10% chance of missing")
+  print("D-Dodge: 75% chance of dodging all damage")
+  print("M-Medkit: Restores you to full health")
+  print("H-Help: Display this help message")
