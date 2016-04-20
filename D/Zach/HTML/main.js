136ff63ecbcbabcc5267d625918e16b5d92483c6
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-19 20:01:34 -0400

    Update main.js

--- D/Zach/HTML/main.js
+++ D/Zach/HTML/main.js
@@ -1,3 +1,11 @@
-function command_info()
+function command_info() // @void
 {
+  writeln("H - help");
+  writeln("W - North");
+  writeln("A - West");
+  writeln("S - South");
+  writeln("D - East");
+  writeln("Q - Up");
+  writeln("Z - Down");
 }
+
