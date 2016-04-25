ab7524d8b1eafd813e1ea839487b1df70acb0b96
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-25 15:18:39 -0400

    Update main.cpp

--- C++/Projects/Journey of the Gods/main.cpp
+++ C++/Projects/Journey of the Gods/main.cpp
@@ -5,6 +5,20 @@
 #include <fstream> //We need to start feature planning from the start! - Saving
 #include <vector>  //For infinite arrays
 using namespace std;
+bool exists (string name) //Tell if file exists
+{
+    ifstream f(name.c_str());
+    if (f.good())
+    {
+        f.close();
+        return true;
+    }
+    else
+    {
+        f.close();
+        return false;
+    }   
+}
 int main(){
   
 }
