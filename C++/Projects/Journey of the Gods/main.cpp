6f9cea57e4b73605a60352c1722ddf08176efe17
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-25 15:17:05 -0400

    Feature Planning Ahead of timee
    @porgull, apparently you can't use vector<int[4]>. It needs vector<int*>, sadly. D IS SUPERIOR!

--- C++/Projects/Journey of the Gods/main.cpp
+++ C++/Projects/Journey of the Gods/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream> //We need to start feature planning from the start! - Saving
+#include <vector>  //For infinite arrays
 using namespace std;
 int main(){
   
