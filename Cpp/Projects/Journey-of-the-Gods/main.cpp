a0dd83ba0273e45a2af3e5903752797e4f319a2f
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-04-28 15:00:49 -0400

    Update main.cpp

--- Cpp/Projects/Journey-of-the-Gods/main.cpp
+++ Cpp/Projects/Journey-of-the-Gods/main.cpp
@@ -7,42 +7,37 @@
 #define KEY_DOWN 115
 #define KEY_LEFT 97
 #define KEY_RIGHT 100
-int x,y;
 using namespace std;
 void move() //Just a test. DON'T MESS WITH THIS
 {
-  int c = 0, d=0;
+  int c = 0, d=0, x=0, y=0;
 	while(1)
 	{
   	c = 0;
 	  d=0;
 	  d=getch();
-	  if(d==224)
-	  {
-    		switch((c=getch())) 
+    switch((c=getch())) 
 		{
-   			case KEY_UP:
-       				x++;    //key up
-       				break;
-   			case KEY_DOWN:
-       				x--;   // key down
-       				break;
-   			case KEY_LEFT:
-       				y++;  // key left
-       				break;
-   			case KEY_RIGHT:
-       				y--;  // key right
-       				break;
-   			default:
-       				cout << endl << "null" << endl;  // not arrow
-				break;
+   case KEY_UP:
+       x++;    //key up
+       break;
+   case KEY_DOWN:
+       x--;   // key down
+       break;
+   case KEY_LEFT:
+       y++;  // key left
+       break;
+   case KEY_RIGHT:
+       y--;  // key right
+       break;
+   default:
+       cout << endl << "null" << endl;  // not arrow
+       break;
 		}
-	  }
 	}
 }
 int main()
 {
-  
+   move();
 }
-//WIP
 
