0318c304b93f7202e0d295d94044a3c952b5c3d9
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-04-28 14:01:18 -0400

    Update main.cpp

--- Cpp/Projects/Journey-of-the-Gods/main.cpp
+++ Cpp/Projects/Journey-of-the-Gods/main.cpp
@@ -2,7 +2,40 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <conio.h>
+#define KEY_UP 119
+#define KEY_DOWN 115
+#define KEY_LEFT 97
+#define KEY_RIGHT 100
 using namespace std;
+void move() //Just a test. DON'T MESS WITH THIS
+{
+  int c = 0, d=0;
+	while(1)
+	{
+  	c = 0;
+	  d=0;
+	  d=getch();
+    switch((c=getch())) 
+		{
+   case KEY_UP:
+       x++;    //key up
+       break;
+   case KEY_DOWN:
+       x--;   // key down
+       break;
+   case KEY_LEFT:
+       y++;  // key left
+       break;
+   case KEY_RIGHT:
+       y--;  // key right
+       break;
+   default:
+       cout << endl << "null" << endl;  // not arrow
+       break;
+		}
+	}
+}
 int main()
 {
   
