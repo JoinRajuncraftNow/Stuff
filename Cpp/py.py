12a16ea1773db205d032fcd8d3d479b8d57b7aa3
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-04 08:41:00 -0400

    Update py.py

--- Cpp/py.py
+++ Cpp/py.py
@@ -1,1 +1,13 @@
-
+try:
+  import readchar
+  has_r = True
+except:
+  import os
+  has_r = False
+def key():
+  if has_r:
+    f=open("temp.txt","w")
+    f.write(ord(readchar.readchar()))
+    f.close()
+  else:
+    os.system("read -n 1 c; echo $c>temp.txt;echo -e -n '\\b'")
