7eb40ffe0a04a6e2a8d2a40a94f62652223bfdc4
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-04 08:47:45 -0400

    Update py.py

--- Cpp/py.py
+++ Cpp/py.py
@@ -6,9 +6,15 @@ except:
   has_r = False
 def key():
   if has_r:
-    f=open("temp.txt","w")
+    f=open("c.txt","w")
     f.write(ord(readchar.readchar()))
     f.close()
   else:
     os.system("read -n 1 c; echo $c>temp.txt;echo -e -n '\\b'")
+    f=open("temp.txt","r")
+    u=ord(f.read()[0])
+    f.close()
+    f=open("c.txt","w")
+    f.write(str(u))
+    f.close()
 key()
