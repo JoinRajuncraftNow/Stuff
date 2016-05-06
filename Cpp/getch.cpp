55d832b8e89b0f0d3f03aa6a71d456112c9e7990
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-05 20:36:08 -0400

    IT'S ALIVE

--- Cpp/getch.cpp
+++ Cpp/getch.cpp
@@ -18,17 +18,42 @@ int key()
 #ifdef __linux__
 #include <fstream>
 #include <cstdlib>
+#include <vector>
+#include <iostream>
 //Python must be compiled with readchar, or read -n must work
 int key()
 {
     //system("python -c 'import readchar;u=ord(readchar.readchar());f=open(\"c.txt\",\"w\");f.write(str(u));f.close()'"); //Obviously very hacked together
-    system("python py.py;wait");
-    ifstream i;
-    i.open("c.txt");
-    int u;
-    i >> u;
-    i.close();
-    return u;
+    static vector<int> reserve;
+         ifstream i;
+         i.open("c.txt");
+         int u;
+    if(reserve.size()==0)
+    {
+         system("python py.py;wait");
+         while(!(i.eof()))
+         {
+               i >> u;
+               reserve.push_back(u);
+         }
+    }
+        u=reserve[0];
+        reserve.erase(reserve.begin());
+        i.close();
+		ofstream o;
+		o.open("c.txt",ios::trunc);
+		int j=0;
+		while(j != reserve.size())
+		{
+			o << reserve[j] << " ";
+			j++;
+		}
+		o.close();
+		if(reserve.size()==1)
+		{
+			reserve.erase(reserve.begin());
+		}
+        return u;
 }
 #endif
 
