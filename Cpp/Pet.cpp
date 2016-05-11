058a1e8b8ec0894684cd757aadff2cb789056801
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-05-11 15:16:46 -0400

    I hate the spaces now

--- Cpp/Pet.cpp
+++ Cpp/Pet.cpp
@@ -2,9 +2,7 @@
 #include <string>
 #include <ctime>
 #include <fstream>
-
 using namespace std;
-
 int read()
 {
 	int t=time(0);
@@ -12,20 +10,17 @@ int read()
 	in >> n;
 	return age + (t-n);
 }
-
 void output(int age)
 {
 	int t=time(0);
 	ofstream out("data.txt");
 	out << t << endl << age;
 }
-
 void feed(int& hunger)
 {
 	cout << "You give the pet food.";
 	hunger=0;
 }
-
 int main()
 {
 	int health=1000, sick=0, hunger=0, age=0;
