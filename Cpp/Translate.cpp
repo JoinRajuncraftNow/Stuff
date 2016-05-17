ef2de414ff6db9adf9102b7e7ea65886d27d81ec
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-05-17 11:30:05 -0400

    Update Translate.cpp

--- Cpp/Translate.cpp
+++ Cpp/Translate.cpp
@@ -1,6 +1,23 @@
-//This is going to be a translator for a code I made.
-//Try to figure it out:
-//Yru yp gigurr oy puy:
- 
- //Good Luck
- //Hoof :ucl
+/*#include <iostream>
+#include <string>
+using namespace std;
+//Not going to make seperate functions because this is so short.
+int main()
+{
+ int x, y;
+ string str, test="qwertyuiop[asdfghjkl;zxcvbnm,QWERTYUIOP{ASDFGHJKL:ZXCVBNM<"
+ cout << "[1-Translate to/2-Translate from]" << endl;
+ cin >> x;
+ if(x==1)
+ {
+  
+ }else{
+  if(x==2)
+  {
+   
+  }else{
+   cout<<"Get away from here, retard!"
+  }
+ }
+}
+*/
