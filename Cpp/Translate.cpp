db051ce79e5202248191fdf596cacf116d7cfece
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-18 09:30:25 -0400

    Semicolons, uncomment, else if

--- Cpp/Translate.cpp
+++ Cpp/Translate.cpp
@@ -1,23 +1,24 @@
-/*#include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 //Not going to make seperate functions because this is so short.
 int main()
 {
- int x, y;
- string str, test="qwertyuiop[asdfghjkl;zxcvbnm,QWERTYUIOP{ASDFGHJKL:ZXCVBNM<"
+ int x=0, y=0; //So no possible errors and stuff
+ string str, test="qwertyuiop[asdfghjkl;zxcvbnm,QWERTYUIOP{ASDFGHJKL:ZXCVBNM<";
  cout << "[1-Translate to/2-Translate from]" << endl;
  cin >> x;
  if(x==1)
  {
   
- }else{
-  if(x==2)
-  {
-   
-  }else{
-   cout<<"Get away from here, retard!"
-  }
  }
+ else if(x==2)
+ {
+ 
+ }
+ else
+ {
+  cout<<"Get away from here, retard!";
+ }
+ return 0;
 }
-*/
