6bec55ab5074651fa8e9ae6611352e0f44b3f84e
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-09-09 10:53:05 -0400

    Update Prime.cpp

--- Cpp/Prime.cpp
+++ Cpp/Prime.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 int main()
 {
-  
+	int input, test, prime=1;
+	cin >> input;
+	test=input;
+	while(test>0)
+	{
+		if(input%test==0) //If I remember correctly, that's how you do it.
+		{
+			prime=0;
+			cout << endl << test;
+		}
+		test--;
+	}
+	if(prime=1)
+	{
+		cout << "The number is Prime!";
+	}
 }
