e3ed851a340a426d7b2ca7c9a61f3045c7cdff73
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-09-09 11:12:25 -0400

    Good enough, just don't put in numbers too big... Or letters...

---
+++ Cpp/Primesmall.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std;int main(){int x, y, z=1;cin>>x;y=x;while(y>0){if(x%y==0&&y!=x&&y!=1){z=0;cout<<endl<<y;}y--;}if(z==1){cout<<endl<<"Prime";}cout<<endl;main();}
