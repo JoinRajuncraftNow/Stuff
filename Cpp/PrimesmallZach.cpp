e312c8d2c8f32e4e09c8a8ac58017b6ede95171e
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-09 17:08:02 -0400

    Update PrimesmallZach.cpp

--- Cpp/PrimesmallZach.cpp
+++ Cpp/PrimesmallZach.cpp
@@ -1,2 +1,2 @@
 #include <iostream>
-int main(){int x,y,z=1;std::cin>>x;for(y=x;y>0;y--)if(x%y==0&&y-x&&y-1)z=0,std::cout<<'\n'<<y;if(z)std::cout<<"\nPrime\n";main();}
+int main(){int x,y,z=1;std::cin>>x;for(y=x;y>1;y--)if(x%y==0&&y-x)z=0,std::cout<<'\n'<<y;if(z)std::cout<<"\nPrime\n";main();}
