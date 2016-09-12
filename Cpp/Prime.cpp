aead05b93d5e43141291067827f07e73a7eafc42
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-09-12 10:57:33 -0400

    Update Prime.cpp

--- Cpp/Prime.cpp
+++ Cpp/Prime.cpp
@@ -3,15 +3,29 @@ using namespace std;
 int main()
 {
 	int input, test, prime=1;
+	int inputt, testt, primet=1;
 	cin >> input;
 	test=input;
 	while(test>0)
 	{
-		if(input%test==0&&test!=input&&test!=1) //If I remember correctly, that's how you do it.
+		if(input%test==0&&test!=input&&test!=1) 
 		{
-			prime=0;
-			cout << endl << test;
+			primet=0;
+			inputt=test;
+			while(testt>0)
+			{
+				if(inputt%testt==0&&testt!=inputt&&testt!=1)
+				{
+					primet=0;
+				}
+				testt--;
+			}
+			if(primet==1)
+			{
+				cout << endl << test;
+			}
 		}
+		cin>>prime; //To pause
 		test--;
 	}
 	if(prime==1)
