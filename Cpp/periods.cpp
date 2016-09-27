f5ed8dcfed7042ef91dac67b7ad9e481f19edd3b
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-09-27 11:08:51 -0400

    Update periods.cpp

--- Cpp/periods.cpp
+++ Cpp/periods.cpp
@@ -20,5 +20,20 @@ main()
 {
 	cin.ignore(10000,'\n');
 	cout << day_seconds();
+	if(day_seconds()<35700){
+		cout << "There are " << 35700-day_seconds() << " seconds until 2nd period!" << endl;
+	}else if(day_seconds()<42000){
+		cout << "There are " << 42000-day_seconds() << " seconds until 2nd period!" << endl;
+	}else if(day_seconds()<48000){
+		cout << "There are " << 48000-day_seconds() << " seconds until 2nd period!" << endl;
+	}else if(day_seconds()<55200){
+		cout << "There are " << 55200-day_seconds() << " seconds until 2nd period!" << endl;//I will do conversions later, just testing now.
+	}
 	return 0;
 }
+/* PERIODS
+9:55 - 35,700
+11:40 - 42,000
+1:20 - 48,000
+3:20 - 55,200
+*/
