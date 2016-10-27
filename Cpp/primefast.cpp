e01caa0ff56cb2160a725479d59e49f099888226
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-10-27 11:15:29 -0400

    Create primefast.cpp

---
+++ Cpp/primefast.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <cmath>
+using namespace std;
+bool testprime(int input) //A simpler and faster prime function than I normally make.
+{
+  if(input%2==0){ return false; }
+	bool prime=true;
+	input=ceil(sqrt(input)); //Square root input, then round up.
+	int test=input;
+	while(test>0) //Test for every value < input, and see if input is divisible.
+	{
+		if(input%test==0 && test!=input && test!=1) 
+		{
+			prime=false;
+		}
+		test--;
+	}
+	return prime;
+}
+int main() //main function
+{
+	int num;
+	cin >> num; //User input into integer 'num'
+	cout << testprime(num) << endl; //Output result of testprime
+}
